85776560a112e38cd38f9118e7fb39b8af84e978
miladymakerparty <132958472+miladymakerparty@users.noreply.github.com>
2023-05-22 00:54:38 -0400

    update

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -9,7 +9,7 @@ import { MintSection } from "~/components/MintSection";
 import { Card } from "~/components/Card";
 import { Status } from "~/components/Status";
 import { Accounts } from "~/components/Accounts";
-import { FaHome, FaUser, FaEnvelope, FaEthereum, FaShip, FaWater, FaTwitter } from 'react-icons/fa';
+import { FaHome, FaUser, FaEnvelope, FaEthereum, FaShip, FaWater, FaTwitter, FaBook } from 'react-icons/fa';
 
 
 
@@ -159,6 +159,9 @@ export default function Index() {
         {/* Content that can scroll */}
       </div>
       <div style={{ display: 'flex', justifyContent: 'center', padding: '16px' }}>
+      <a href="https://etherscan.io/address/0x05C63282c87f620aF5a658cBb53548257F3A6186">
+          <FaBook size={48} color="purple"/>
+        </a>
         <a href="https://etherscan.io/address/0x05C63282c87f620aF5a658cBb53548257F3A6186">
           <FaEthereum size={48} color="purple"/>
         </a>
