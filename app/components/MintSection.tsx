b20000a4c9ba8e9e30a67361c7ccf01d155257b4
miladymakerparty <132958472+miladymakerparty@users.noreply.github.com>
2023-05-21 22:51:35 -0400

    update contract

--- app/components/MintSection.tsx
+++ app/components/MintSection.tsx
@@ -9,7 +9,7 @@ export const ETHERSCAN_DOMAIN = "https://etherscan.io";
 export const API_URL = "https://scatter-api.fly.dev";
 // export const API_URL = "https://scatter-api-testing.fly.dev";
 
-const address = "0xc62E3fd5B02618f90dD07d1E478963038fA9089c"; // contract address 
+const address = "0x05C63282c87f620aF5a658cBb53548257F3A6186"; // contract address 
 const root =
   "0x66e915aa20ae05002bc22942644e1520cf77a628aefadff9248f0b42c4d80bd0"; // what
 
