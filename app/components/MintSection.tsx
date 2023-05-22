1f27a17da777f826c66eeaeedc8c84dda0f585ba
miladymakerparty <132958472+miladymakerparty@users.noreply.github.com>
2023-05-22 11:59:14 -0400

    update

--- app/components/MintSection.tsx
+++ app/components/MintSection.tsx
@@ -10,8 +10,7 @@ export const API_URL = "https://scatter-api.fly.dev";
 // export const API_URL = "https://scatter-api-testing.fly.dev";
 
 const address = "0x05C63282c87f620aF5a658cBb53548257F3A6186"; // contract address 
-const root =
-  ""; // removed
+const root = "0x66915aa20ae05002bc22942644e1520cf77a628aefadff9248f0b42c4d80bdg"; // removed
 
 const abi = require("../lib/abi/ArchetypeV51.json");
 
